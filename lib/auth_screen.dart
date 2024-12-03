cc4f5753f57f976bf52321526ee213583bb57ff0
priyesh-techabbot <priyesh.bhuva@techabbot.com>
2024-12-03 14:54:48 +0530

    github login

--- lib/auth_screen.dart
+++ lib/auth_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_social_login/loader.dart';
+import 'package:github_oauth/github_oauth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:twitter_login/twitter_login.dart';
 
@@ -47,11 +48,11 @@ class _AuthScreenState extends State<AuthScreen> {
                       border: Border.all(color: Colors.black),
                       borderRadius: BorderRadius.circular(30),
                     ),
-                    child:  Row(
+                    child: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         const SizedBox(width: 10),
-                         Image.asset(
+                        Image.asset(
                           'assets/X_logo.jpg',
                           height: 25,
                         ),
@@ -97,6 +98,36 @@ class _AuthScreenState extends State<AuthScreen> {
                     ),
                   ),
                 ),
+                const SizedBox(height: 12),
+                InkWell(
+                  onTap: signInWithGitHub,
+                  child: Container(
+                    width: width / 2,
+                    padding: const EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                      color: Colors.purple,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset(
+                          'assets/github.png',
+                          height: 25,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(width: 10),
+                        const Text(
+                          "Signin With Github",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
@@ -116,8 +147,7 @@ class _AuthScreenState extends State<AuthScreen> {
       final twitterLogin = TwitterLogin(
           apiKey: 'EP4JF7B7y8q4JFBCONrLAM8XN',
           apiSecretKey: 'sdRKurpBeAUCdBV7F4IznqlJcKnN33hvvnPOP29gr4qbiIzjil',
-          redirectURI:
-              'socialflutterdemo://');
+          redirectURI: 'socialflutterdemo://');
 
       // Trigger the sign-in flow
       final authResult = await twitterLogin.login();
@@ -162,6 +192,45 @@ class _AuthScreenState extends State<AuthScreen> {
         );
 
         await FirebaseAuth.instance.signInWithCredential(credential);
+      } else {
+        isLoading = false;
+        setState(() {});
+      }
+    } catch (e, s) {
+      isLoading = false;
+      setState(() {});
+
+      print(e);
+      print(s);
+    }
+  }
+
+  Future<void> signInWithGitHub() async {
+    try {
+      isLoading = true;
+      setState(() {});
+      // Create a GitHubSignIn instance
+      final GitHubSignIn gitHubSignIn = GitHubSignIn(
+          clientId: 'Ov23liWt0SNuzdFsVRSG',
+          clientSecret: 'c9030b01cf56e0bf2ed10145dabb90a96ed101f5',
+          redirectUrl:
+              'https://flutter-social-login-1f174.firebaseapp.com/__/auth/handler');
+
+      // Trigger the sign-in flow
+      final result = await gitHubSignIn.signIn(context);
+
+      if (result.token != null) {
+        // Create a credential from the access token
+        final githubAuthCredential =
+            GithubAuthProvider.credential(result.token!);
+
+        // Once signed in, return the UserCredential
+        await FirebaseAuth.instance.signInWithCredential(githubAuthCredential);
+
+        if (result.userProfile!['name'] == null) {
+          await FirebaseAuth.instance.currentUser
+              ?.updateDisplayName(result.userProfile!['login']);
+        }
       } else {
         isLoading = false;
         setState(() {});
